b912bd146f73a9acf4640c9d0abf250d9a59f496
Ruan Kenned <ruankennedm@gmail.com>
2022-10-07 22:29:33 -0300

    Finalizando módulo de requisição

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento: Pensamento = {
+    conteudo: 'Ruan',
+    autoria: 'Ruan',
+    modelo: 'modelo2',
+    id: 0
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento (): string{
+    if(this.pensamento.conteudo.length > 256){
+      return 'pensamento-g';
+    }else{
+      return 'pensamento-p';
+    }
+  }
+
+}
